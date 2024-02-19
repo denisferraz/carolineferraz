f85a52fff3dacf337f934a7322763585bbaa15fc
Denis Ferraz <denis_ferraz359@hotmail.com>
2024-02-19 07:40:34 -0300

    phpspreadseet

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit056cb97e53152ee35af26e0c54323136
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit056cb97e53152ee35af26e0c54323136::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
